0e67aca784b1ce82aadcdede3fba35f157ca1952
Zoltan Rado <shared.ptr@gmail.com>
2017-12-02 13:08:28 +0100

    ...

--- elliptic.rkt
+++ elliptic.rkt
@@ -5,20 +5,20 @@
 (define bc_E_b 0)
 (define bc_E_c 7)
 (define bc_G 0)
-(define bc_p 0)
+(define bc_p 1)
 
 ;(define (mod_pow x n p)
 ;)
 ;(define (pow_exp r n)
 ;)
 
-(define (pow_exp x n)
+(define (pow_exp x n mod_prime)
   (let pow_exp_aux ([carry 1]
                     [result 1]
                     [r0 x]
                     [n n]
                     )
-    (let ([carry (if (eq? carry 1) r0 (sqr carry))]
+    (let ([carry (if (eq? carry 1) r0 (mod_prime (sqr carry)))]
           [>> (lambda (x) (arithmetic-shift x -1))])
       (printf "pow_exp carry: ~a result: ~a r0: ~a n: ~a~n" carry result r0 n)
       (cond
@@ -26,10 +26,14 @@
         (else
           (if (eq? (modulo n 2) 0)
             (pow_exp_aux carry result r0 (>> n))
-            (pow_exp_aux carry (* carry result) r0 (>> (sub1 n)))
+            (pow_exp_aux carry (mod_prime (* carry result)) r0 (>> (sub1 n)))
             )
           )
         )
       )
     )
   )
+
+(define (pow_bitcoin x n) (pow_exp x n (lambda (x) (modulo x bc_p))))
+(define (pow_p x n p) (pow_exp x n (lambda (x) (modulo x p))))
+(define (pow x n) (pow_exp x n identity))
--- ut.rkt
+++ ut.rkt
@@ -1,11 +1,17 @@
 #lang racket
 (require rackunit "elliptic.rkt")
-(check-eq? (pow_exp 5 0) 1)
-(check-eq? (pow_exp 5 1) 5)
-(check-eq? (pow_exp 5 2) 25)
-(check-eq? (pow_exp 5 3) 125)
-(check-eq? (pow_exp 5 4) (* 5 125))
+(check-eq? (pow 5 0) 1)
+(check-eq? (pow 5 1) 5)
+(check-eq? (pow 5 2) 25)
+(check-eq? (pow 5 3) 125)
+(check-eq? (pow 5 4) (* 5 125))
 
+;(check-eq? (pow_bitcoin 5 0) 1)
+;(check-eq? (pow_bitcoin 5 1) 5)
+;(check-eq? (pow_bitcoin 5 2) 25)
+;(check-eq? (pow_bitcoin 5 3) 125)
+;(check-eq? (pow_bitcoin 5 4) (* 5 125))
+;
 ;(check-eq? (pow_exp 5 0) 1)
 ;(check-eq? (pow_exp 5 1) 5)
 ;(check-eq? (pow_exp 5 2) 25)
