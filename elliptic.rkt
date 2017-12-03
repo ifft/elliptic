2f2e99338dc27722051f4ac294488f7a9d9c4915
Zoltan Rado <shared.ptr@gmail.com>
2017-12-03 15:34:40 +0100

    factor-2 one unit test. Bitcoin curve parameters added

--- elliptic.rkt
+++ elliptic.rkt
@@ -1,11 +1,12 @@
 #lang racket
 (provide (all-defined-out))
 ;bitcoin's elliptic curve parameters
-(define bc_E_a 1)
-(define bc_E_b 0)
-(define bc_E_c 7)
-(define bc_G 0)
-(define bc_p 1)
+(define bc_E_a 0)
+(define bc_E_b 7)
+(define bc_p #xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)
+(define bc_G #x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798)
+(define bc_G_long #x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
+(define bc_n #xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141)
 
 ;(define (mod_pow x n p)
 ;)
@@ -34,6 +35,15 @@
     )
   )
 
+(define (factor-2 x q)
+ (cond
+  ((eq? (modulo x 2) 0) (factor-2 (quotient x 2) (add1 q)))
+  (else (values q x))
+ )
+)
+
+
+
 (define (pow_bitcoin x n) (pow_exp x n (lambda (x) (modulo x bc_p))))
-(define (pow_p x n p) (pow_exp x n (lambda (x) (modulo x p))))
+(define (pow-p x n p) (pow_exp x n (lambda (x) (modulo x p))))
 (define (pow x n) (pow_exp x n identity))
--- ut.rkt
+++ ut.rkt
@@ -1,10 +1,19 @@
 #lang racket
 (require rackunit "elliptic.rkt")
+
+;;;;; pow ;;;;;
 (check-eq? (pow 5 0) 1)
 (check-eq? (pow 5 1) 5)
 (check-eq? (pow 5 2) 25)
 (check-eq? (pow 5 3) 125)
 (check-eq? (pow 5 4) (* 5 125))
+
+;;;;; factor-2 ;;;;;
+(define (get-q q s) q)
+(define (get-s q s) s)
+
+(check-eq? (call-with-values (lambda () (factor-2 12 0)) get-q) 2)
+(check-eq? (call-with-values (lambda () (factor-2 12 0)) get-s) 3)
 
 ;(check-eq? (pow_bitcoin 5 0) 1)
 ;(check-eq? (pow_bitcoin 5 1) 5)
