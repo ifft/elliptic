9bca2f91c87c6fc598d9b9d0bf48d85bde85fa0c
Zoltan Rado <shared.ptr@gmail.com>
2017-12-28 20:01:54 +0100

    signature test with tampered message

--- ut_crypto.rkt
+++ ut_crypto.rkt
@@ -5,11 +5,14 @@
 (require "bitcoin-curve.rkt")
 (require "modulo-arith.rkt")
 (require "elliptic-arith.rkt")
+
 (with-helper-funcs
   bitcoin-curve
 
   (random-seed 42)
+
   (define message #"Message to be signed")
+  (define tamperedmessage #"Message to be Signed")
   (define alicepriv (gen-priv-key bitcoin-curve))
   (define alicepub (* bc_G alicepriv))
 
@@ -17,7 +20,8 @@
   (check-equal? r 107259175415431880076523644623259231832630027381936395642049712571715173317563)
   (check-equal? s 104460022056839873524628905760380203278361492473266195692294703911620822836515)
 
-  (check-true (verify (n-byte-int->number 32 (padmessage message)) r s alicepub bitcoin-curve))
+  (check-true  (verify (n-byte-int->number 32 (padmessage message)) r s alicepub bitcoin-curve))
+  (check-false (verify (n-byte-int->number 32 (padmessage tamperedmessage)) r s alicepub bitcoin-curve))
   )
 
 ;;;;; sign ;;;;;
